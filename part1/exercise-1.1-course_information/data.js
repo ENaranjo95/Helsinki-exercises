23f6ba8e989be69333d3b3d498fcc940682fba0c
Edwin Naranjo <edwinx.naranjo@gmail.com>
2025-02-01 00:06:54 -0500

    Updated mock data to use an array of object for easy mapping of the text inside of the content component

--- part1/exercise-1.1-course_information/data.js
+++ part1/exercise-1.1-course_information/data.js
@@ -1,9 +1,17 @@
 export default {
     course: 'Half Stack application development',
-    part1: 'Fundamentals of React',
-    exercises1: 10,
-    part2: 'Using props to pass data',
-    exercises2: 7,
-    part3: 'State of a component',
-    exercises3: 14
+    content: [
+        {
+            text: 'Fundamentals of React',
+            exercises: 10
+        },
+        {
+            text: 'Using props to pass data',
+            exercises: 7
+        },
+        {
+            text: 'State of a component',
+            exercises: 14
+        }
+    ],
 };
