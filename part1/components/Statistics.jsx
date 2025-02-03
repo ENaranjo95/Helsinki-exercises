c6fc0b058b0737c25e0ab2f365797b412a4f77b4
Edwin Naranjo <edwinx.naranjo@gmail.com>
2025-02-02 22:29:29 -0500

    Refactor the app component to move the statistics in to it's own component

---
+++ part1/components/Statistics.jsx
@@ -0,0 +1,26 @@
+import StatisticLine from "./StatisticLine";
+const Statistics = (props) => {
+    const { good, neutral, bad } = props;
+
+    const averageScore = good - bad / (good + neutral + bad);
+    const positiveScore = (good / (good + neutral + bad)) * 100;
+
+    if (good === 0 && neutral === 0 && bad === 0) {
+        return <p>No feedback given</p>
+    }
+
+    return (
+        <table>
+            <tbody>
+                <StatisticLine text='good' value={good} />
+                <StatisticLine text='neutral' value={neutral} />
+                <StatisticLine text='bad' value={bad} />
+                <StatisticLine text='all' value={good + neutral + bad} />
+                <StatisticLine text='average' value={averageScore} />
+                <StatisticLine text='positive' value={`${positiveScore}%`} />
+            </tbody>
+        </table>
+    )
+};
+
+export default Statistics;
--- part1/exercise-1.6-unicafe/App.jsx
+++ part1/exercise-1.6-unicafe/App.jsx
@@ -3,26 +3,26 @@ import { useState, useCallback } from 'react'
 import Button from '../components/Button';
 import Header from '../components/Header';
 import Part from '../components/Part';
+import Statistics from '../components/Statistics';
 
 import { createUniqueKey } from '../helper/index';
 import data from './data';
 
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
   const { feedbackOptions } = data;
 
-  const setFeedbackOption = useCallback((text, type = 'button') => {
+  const setFeedbackOption = useCallback((text) => {
     switch(text) {
       case 'good':
-        return type === 'button' ? () => setGood((prevValue) => prevValue + 1) : good;
+        return () => setGood((prevValue) => prevValue + 1);
       case 'neutral':
-        return type === 'button' ? () => setNeutral((prevValue) => prevValue + 1) : neutral;
+        return () => setNeutral((prevValue) => prevValue + 1);
       case 'bad':
-        return type === 'button' ? () => setBad((prevValue) => prevValue + 1) : bad;
+        return () => setBad((prevValue) => prevValue + 1);
     }
   }, [good, neutral, bad]);
 
@@ -32,7 +32,7 @@ const App = () => {
       {feedbackOptions.map((option, index) => (<Button key={createUniqueKey(`${option}-button`, index)} text={option} onClick={setFeedbackOption(option)} />))}
 
       <Header as='h2' name='Statistics' />
-      {feedbackOptions.map((option, index) => (<Part key={createUniqueKey(`${option}-text`, index)} name={option} value={setFeedbackOption(option, 'text')} />))}
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
