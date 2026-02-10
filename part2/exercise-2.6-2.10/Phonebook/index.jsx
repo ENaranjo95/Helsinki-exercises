92c84a64999375a97f763bf0a18165d889cbfa1c
Edwin Naranjo <edwinx.naranjo@gmail.com>
2026-02-09 20:12:03 -0500

    Initial commit adding form submission for the phonebook app. Handling the new name functionality to the phonebook app. Added file name changes to the notes app

---
+++ part2/exercise-2.6-2.10/Phonebook/index.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', id: 1 }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handlePersonChange = (event) => {
+    setNewName(event.target.value);
+  }
+
+  const handlePhonebookSubmission = (event) => {
+    event.preventDefault();
+    const newPerson = {
+        name: newName,
+        id: `${persons.length + 1}`
+    }
+    setPersons(persons.concat(newPerson));
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handlePhonebookSubmission}>
+        <div>
+          name: <input value={newName} onChange={handlePersonChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person) => <li key={person.id}>{person.name}</li>)}
+      </ul>
+    </div>
+  )
+}
+
+export default App
