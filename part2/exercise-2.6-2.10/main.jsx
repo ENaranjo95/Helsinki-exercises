a796bcad489993d57dca77df67a1c85510c6b931
Edwin Naranjo <edwinx.naranjo@gmail.com>
2026-04-23 08:09:07 -0400

    Updated notes exercise from reading. Added a useEffect hook to make API call to notes server

--- part2/exercise-2.6-2.10/main.jsx
+++ part2/exercise-2.6-2.10/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App';
 
+
 export default () => createRoot(document.getElementById('part2Apps')).render(
   <StrictMode>
     <App />
